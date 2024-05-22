dcdfb1502832b95e8105697aa979948670af81d4
Chandana <kurnutala.chandana@capgemini.com>
2024-05-22 15:42:46 +0530

    committed code in branch2

--- src/main/java/com/cg/demo/GitRepositoryDemoApplication.java
+++ src/main/java/com/cg/demo/GitRepositoryDemoApplication.java
@@ -8,6 +8,7 @@ public class GitRepositoryDemoApplication {
 
 	public static void main(String[] args) {
 	System.out.println("entered into GitRepositoryDemoApplication");
+	System.out.println("added branch through git cli");
 		SpringApplication.run(GitRepositoryDemoApplication.class, args);
 	}
 
