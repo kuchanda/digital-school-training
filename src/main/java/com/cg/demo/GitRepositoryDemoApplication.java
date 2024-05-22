0e0c50b25a2eb0fd4e22de58a3e647796d51b7c5
Chandana <kurnutala.chandana@capgemini.com>
2024-05-22 15:01:21 +0530

    initial commit

---
+++ src/main/java/com/cg/demo/GitRepositoryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepositoryDemoApplication.class, args);
+	}
+
+}
