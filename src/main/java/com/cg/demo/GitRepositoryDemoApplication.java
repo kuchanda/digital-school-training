a2f74d94702e5553bd428c73e8127d84405c3338
Kurnutala Chandana <kurnutala.chandana@capgemini.com>
2024-05-22 15:10:49 +0530

    Update GitRepositoryDemoApplication.java

--- src/main/java/com/cg/demo/GitRepositoryDemoApplication.java
+++ src/main/java/com/cg/demo/GitRepositoryDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitRepositoryDemoApplication {
 
 	public static void main(String[] args) {
+	System.out.println("entered into GitRepositoryDemoApplication");
 		SpringApplication.run(GitRepositoryDemoApplication.class, args);
 	}
 
